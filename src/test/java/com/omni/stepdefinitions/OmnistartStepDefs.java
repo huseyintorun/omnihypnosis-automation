b0e67d54d8a0cbecf7b45fa0a0263b3d39c242f2
huseyintorun <85924424+huseyintorun@users.noreply.github.com>
2024-06-28 08:30:24 +0200

    Update OmnistartStepDefs.java
    Update the positions of Step Definitions

--- src/test/java/com/omni/stepdefinitions/OmnistartStepDefs.java
+++ src/test/java/com/omni/stepdefinitions/OmnistartStepDefs.java
@@ -45,8 +45,6 @@ public class OmnistartStepDefs {
 
     @When("the user clicks on euRecht accepted")
     public void theUserClicksOnEuRechtAccepted() throws InterruptedException {
-   // Alert alert = Driver.get().switchTo().alert();
-    //alert.accept();
       omniLanding.modul1.click();
       Thread.sleep(3000);
     }
@@ -57,101 +55,13 @@ public class OmnistartStepDefs {
         Thread.sleep(2000);
     }
 
-    @When("the user click on Ausbildung")
-    public void the_user_click_on_Ausbildung() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("The user on Ausbildung")
-    public void the_user_on_Ausbildung() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @When("the user click on Weiterbildung")
-    public void the_user_click_on_Weiterbildung() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("The user on Weiterbildung")
-    public void the_user_on_Weiterbildung() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
     @When("the user click on OMNIfinder")
     public void the_user_click_on_OMNIfinder() throws InterruptedException {
 
         omniLanding.omniFinder.click();
-        // 20 saniye (20000 milisaniye) uyku
         Thread.sleep(5000);
 
     }
-
-
-    @Then("The user on OMNIfinder")
-    public void the_user_on_OMNIfinder() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @When("the user click on Uber OMNI")
-    public void the_user_click_on_Uber_OMNI() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("The user on Uber OMNI")
-    public void the_user_on_Uber_OMNI() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @When("the user click on Sign In")
-    public void the_user_click_on_Sign_In() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("The user on Sign In")
-    public void the_user_on_Sign_In() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-
-    @When("the user click on OMNI Hypnotisor_Modul1")
-    public void the_user_click_on_OMNI_Hypnotisor_Modul1() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @When("the user click on jetzt Onlineausbildung starten")
-    public void the_user_click_on_jetzt_Onlineausbildung_starten() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @When("user fills the form")
-    public void user_fills_the_form() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @When("user clicks on Debit- oder Kreditkarte")
-    public void user_clicks_on_Debit_oder_Kreditkarte() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("user checks the paypal")
-    public void user_checks_the_paypal() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
 
     @And("the user clicks on BEGINNE NOCH HEUTE starten")
     public void theUserClicksOnBEGINNENOCHHEUTEStarten() throws InterruptedException {
@@ -219,5 +129,88 @@ public class OmnistartStepDefs {
         WebElement phoneNumber = Driver.get().findElement(By.name("phoneNumber"));
         phoneNumber.sendKeys("+491234567890");
         Thread.sleep(1000);
+    }
+      @When("the user click on Ausbildung")
+    public void the_user_click_on_Ausbildung() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The user on Ausbildung")
+    public void the_user_on_Ausbildung() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user click on Weiterbildung")
+    public void the_user_click_on_Weiterbildung() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The user on Weiterbildung")
+    public void the_user_on_Weiterbildung() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+       @Then("The user on OMNIfinder")
+    public void the_user_on_OMNIfinder() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user click on Uber OMNI")
+    public void the_user_click_on_Uber_OMNI() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The user on Uber OMNI")
+    public void the_user_on_Uber_OMNI() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user click on Sign In")
+    public void the_user_click_on_Sign_In() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The user on Sign In")
+    public void the_user_on_Sign_In() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+    @When("the user click on OMNI Hypnotisor_Modul1")
+    public void the_user_click_on_OMNI_Hypnotisor_Modul1() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user click on jetzt Onlineausbildung starten")
+    public void the_user_click_on_jetzt_Onlineausbildung_starten() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user fills the form")
+    public void user_fills_the_form() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user clicks on Debit- oder Kreditkarte")
+    public void user_clicks_on_Debit_oder_Kreditkarte() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user checks the paypal")
+    public void user_checks_the_paypal() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 }
